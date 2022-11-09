e90ac42ca5621d8082474c5827b68c3834a32752
komatr-cz <73248883+komatr-supra@users.noreply.github.com>
2022-11-09 14:48:40 +0100

    Complete?
    function animation... chcek and clean a bit

--- Assets/_Scripts/CharacterAnimator.cs
+++ Assets/_Scripts/CharacterAnimator.cs
@@ -8,32 +8,75 @@ public class CharacterAnimator : MonoBehaviour
     [SerializeField] List<Sprite> frontSprites;
     [SerializeField] List<Sprite> backSprites;
     [SerializeField] float changeTime = 0.1f;
+    Mover mover;
     List<Sprite> actualList;
     float timer;
     SpriteRenderer spriteRenderer;
-    int actualSprite;
+    int actualSpriteIndex;
+    bool mirrowed = false;
+    bool isMoving = false;
     private void Awake()
     {
+        mover = GetComponent<Mover>();
         spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    private void OnEnable()
+    {
+        mover.onMovementTypeChange += ChangeMovementAnimation;
+    }
+    private void OnDisable()
+    {
+        mover.onMovementTypeChange -= ChangeMovementAnimation;
     }
     void Start()
     {
         actualList = sideSprites;
     }
-
+    void ChangeMovementAnimation(Direction direction, bool isMoving)
+    {
+        actualSpriteIndex = 0;
+        this.isMoving = isMoving;
+        switch (direction)
+        {
+            case Direction.right:
+                actualList = sideSprites;
+                mirrowed = false;
+                break;
+            case Direction.top:
+                actualList = backSprites;
+                mirrowed = false;
+                break;
+            case Direction.left:
+                actualList = sideSprites;
+                mirrowed = true;
+                break;
+            case Direction.bot:
+                actualList = frontSprites;
+                mirrowed = false;
+                break;
+            default:
+                break;
+        }
+    }
     
     void Update()
     {
+        spriteRenderer.flipX = mirrowed;
+        if (!isMoving)
+        {
+            spriteRenderer.sprite = actualList[actualSpriteIndex];
+            return;
+        }
         timer += Time.deltaTime;
         if(timer > changeTime)
         {
             timer = 0;
             //change sprite
-            if(++actualSprite == actualList.Count)
+            if(++actualSpriteIndex == actualList.Count)
             {
-                actualSprite = 0;
+                actualSpriteIndex = 0;
             }
-            spriteRenderer.sprite = actualList[actualSprite];
+            spriteRenderer.sprite = actualList[actualSpriteIndex];
         }
         
     }
--- Assets/_Scripts/Mover.cs
+++ Assets/_Scripts/Mover.cs
@@ -1,13 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
+public enum Direction
+{
+    right, top, left, bot
+}
 public class Mover : MonoBehaviour
 {
-    [Header("Using physics, so breaking and moving speed must be set right and tuned!")]
+    [Header("Using physics")]
     [SerializeField] float moveSpeed = 2f;
-    [SerializeField] float brakingSpeed = 5f;
+    [SerializeField] float brakingSpeed = 0f;
     [HideInInspector] public bool canMove = true;
+
+    public Action<Direction, bool> onMovementTypeChange;
+
     Vector2 moveDir = Vector2.zero;
     Vector2 lastMoveDir = Vector2.zero;
     bool isMoving;
@@ -35,28 +43,74 @@ public class Mover : MonoBehaviour
     void Move(Vector2 moveVector)
     {
         moveDir = moveVector;
+        Vector2 compareVectors = new Vector2(
+                MathF.Ceiling(moveDir.x), MathF.Ceiling(moveDir.y));
+        Debug.Log(compareVectors);
+        if(lastMoveDir != compareVectors)
+        {
+            
+            //is moving?
+            if (compareVectors == Vector2.zero)
+            {
+                isMoving = false;
+                onMovementTypeChange?.Invoke(ChooseDirection(), false);
+                lastMoveDir = moveDir;
+            }
+            else
+            {
+                isMoving = false;
+                lastMoveDir = moveDir;            
+                onMovementTypeChange?.Invoke(ChooseDirection(), true);
+            }
+
+            
+        }
+
     }
+
+    private Direction ChooseDirection()
+    {
+        if (lastMoveDir.y > 0.9f)
+        {
+            return Direction.top;
+        }
+        else if (lastMoveDir.y < -0.9f)
+        {
+            return Direction.bot;
+        }
+        else if (lastMoveDir.x > 0.9f)
+        {
+            return Direction.right;
+        }
+        else if (lastMoveDir.x < -0.9f)
+        {
+            return Direction.left;
+        }
+        else return Direction.bot;
+        
+    }
+
     /*
-    void Update()
-    {        
-        if(Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0){
-            moveDir = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-            lastMoveDir = moveDir;            
-            isMoving = true;
-        }
-        else{
-            moveDir = Vector2.zero;
-            isMoving = false;
-        }
-        if(!canMove) {
-            moveDir = Vector2.zero;
-            isMoving = false;
-        }
-        animator.SetBool("isMoving", isMoving);
-        animator.SetFloat("xValue", lastMoveDir.x);
-        animator.SetFloat("yValue", lastMoveDir.y);
-    }
-    */
+void Update()
+{        
+   if(Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0){
+       moveDir = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+       lastMoveDir = moveDir;            
+       isMoving = true;
+   }
+   else{
+       moveDir = Vector2.zero;
+       isMoving = false;
+   }
+   if(!canMove) {
+       moveDir = Vector2.zero;
+       isMoving = false;
+   }
+   animator.SetBool("isMoving", isMoving);
+   animator.SetFloat("xValue", lastMoveDir.x);
+   animator.SetFloat("yValue", lastMoveDir.y);
+}
+*/
     private void FixedUpdate() {
         if(canMove)        
         rb2d.velocity = moveDir * moveSpeed;
